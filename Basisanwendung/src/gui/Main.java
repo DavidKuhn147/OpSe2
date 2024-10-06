a40b1c676f2ea536ba9ee922b342a02391c42e3f
David <David@DESKTOP-2MKPUL0>
2024-10-06 17:07:17 +0200

    InitialB

--- Basisanwendung/src/gui/Main.java
+++ Basisanwendung/src/gui/Main.java
@@ -1,30 +1,10 @@
 package gui;
-
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
-import javafx.stage.Stage;
-
-public class Main extends Application{
-
+import business.CsvDateiLeser;
+public class Main {
+	
 	public static void main(String[] args) {
-		System.out.print("Guten Tagal");
-		launch(args);
+		CsvDateiLeser cdl = new CsvDateiLeser();
+		String ueberschrift = cdl.getUeberschrift();
+		System.out.println(ueberschrift);
 	}
-
-	@Override
-	public void start(Stage st) throws Exception {
-		st.setTitle("TestWindow");
-		
-		Pane pane = new Pane();
-		
-		Scene scene = new Scene(pane,500,300);
-		
-		
-		st.setScene(scene);
-		st.show();
-		
-		
-	}
-
 }
