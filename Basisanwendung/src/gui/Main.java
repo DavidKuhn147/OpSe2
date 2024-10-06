63a5c24e85fd28f2e3b989310f5de6c7ccb1bc14
David <David@DESKTOP-2MKPUL0>
2024-10-06 16:58:30 +0200

    InitialA

--- Basisanwendung/src/gui/Main.java
+++ Basisanwendung/src/gui/Main.java
@@ -1,30 +1,15 @@
 package gui;
-
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-public class Main extends Application{
 
-	public static void main(String[] args) {
-		System.out.print("Guten TagAAAAAA");
-		launch(args);
-	}
+public class Main extends Application {
+	 @Override
+	 public void start(Stage primaryStage) {
+		 new Anwendersystem(primaryStage);
+	 }
 
-	@Override
-	public void start(Stage st) throws Exception {
-		st.setTitle("TestWindow");
-		
-		Pane pane = new Pane();
-		
-		Scene scene = new Scene(pane,500,300);
-		
-		
-		st.setScene(scene);
-		st.show();
-		
-		
-	}
-
+	 public static void main(String[] args){
+		 launch(args);
+	 }
 }
