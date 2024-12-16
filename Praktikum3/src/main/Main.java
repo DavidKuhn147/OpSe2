c4614c929d1b9539c33a79a86f96ab7dacb1c691
DavidKuhn147 <david.k.14@web.de>
2024-12-16 01:52:06 +0100

    Test Upload Git

--- Praktikum3/src/main/Main.java
+++ Praktikum3/src/main/Main.java
@@ -7,7 +7,7 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-	
+	//GitHub TEST
 	@Override
 	public void start(Stage primaryStage) {
 		//Aufruf der Vorgabe, um Buerger aufzunehmen/speichern und sie in eine txt/csv Datei zu schreiben
